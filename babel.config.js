195abf6db32b6ba8ccd2092cbe2442d744dbc73b
Marcus Balbi <balbimarcus@gmail.com>
2021-05-02 14:16:30 -0300

    trying to fix import/export vs module.exports

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sourceType: "unambiguous",
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: ["@babel/plugin-transform-runtime"],
+  // targets: "defaults",
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
