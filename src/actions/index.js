8dd6567add683251162e252f7481bff2df15190d
Marcus Balbi <balbimarcus@gmail.com>
2021-05-03 20:47:55 -0300

    change channelid 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,7 @@ import {
   RESET_SELECTION
 } from './types';
 
+
 import GetAgeRange from '../../database/queries/GetAgeRange';
 import GetYearsActiveRange from '../../database/queries/GetYearsActiveRange';
 import SearchArtists from '../../database/queries/SearchArtists';
