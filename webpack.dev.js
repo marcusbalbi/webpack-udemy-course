c6ac7e0a6fc8c954fdee32000bee04c4e8d003fc
Marcus Balbi <balbimarcus@gmail.com>
2021-05-03 19:02:20 -0300

    split config

---
+++ webpack.dev.js
@@ -0,0 +1,8 @@
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.config");
+
+const config = {
+  mode: "development",
+};
+
+module.exports = merge(baseConfig, config);
